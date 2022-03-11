23b0392740c022fa38ec11ee9fd5142221e20838
anddev404 <marcinek07@gmail.com>
2022-03-11 18:23:32 +0100

    add news to MainViewModel

--- app/src/main/java/com/anddev404/technews/MainViewModel.kt
+++ app/src/main/java/com/anddev404/technews/MainViewModel.kt
@@ -1,8 +1,21 @@
 package com.anddev404.technews
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.anddev404.repository.Repository
+import com.anddev404.repository.model.News
 
 class MainViewModel(val repository: Repository) : ViewModel() {
-    
+
+    private val _news =
+        MutableLiveData<News>()
+
+    fun getNews(): LiveData<News> {
+        return _news
+    }
+
+    fun setNews(news: News) {
+        _news.value = news
+    }
 }
