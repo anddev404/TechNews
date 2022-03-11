fcfe9c394b22fda871b95fbb3dad33d220f6e76e
anddev404 <marcinek07@gmail.com>
2022-03-11 18:22:38 +0100

    Create MainViewModel.kt

---
+++ app/src/main/java/com/anddev404/technews/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.anddev404.technews
+
+import androidx.lifecycle.ViewModel
+import com.anddev404.repository.Repository
+
+class MainViewModel(val repository: Repository) : ViewModel() {
+    
+}
