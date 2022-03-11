e930353dc56573f4b8365ac8d32bc8d28cb34c35
anddev404 <marcinek07@gmail.com>
2022-03-11 18:26:21 +0100

    add fun downloadNews to MainViewModel

--- app/src/main/java/com/anddev404/technews/MainViewModel.kt
+++ app/src/main/java/com/anddev404/technews/MainViewModel.kt
@@ -1,10 +1,15 @@
 package com.anddev404.technews
 
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.anddev404.repository.Repository
 import com.anddev404.repository.model.News
+import com.anddev404.repository.remote.ApiSource
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainViewModel(val repository: Repository) : ViewModel() {
 
@@ -17,5 +22,20 @@ class MainViewModel(val repository: Repository) : ViewModel() {
 
     fun setNews(news: News) {
         _news.value = news
+    }
+
+    fun downloadNews() {
+
+        var api = repository.getApi(ApiSource.TECH_NEWS)
+
+        GlobalScope.launch {
+
+            var newsList = api.getNewsOrEmptyList()
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    setNews(newsList)
+                }, 0
+            )
+        }
     }
 }
