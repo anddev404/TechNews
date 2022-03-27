ac2b8306e149cbe5ad8ca2015e6103c239f6174a
anddev404 <marcinek07@gmail.com>
2022-03-27 18:06:07 +0200

    add fun to check network

---
+++ app/src/main/java/com/anddev404/technews/utils/Internet.kt
@@ -0,0 +1,42 @@
+package com.anddev404.technews.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+class Internet {
+    companion object {
+
+        fun isOnline(context: Context): Boolean {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (connectivityManager != null) {
+                val capabilities =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                    } else {
+
+                        val manager =
+                            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                        val networkInfo = manager.activeNetworkInfo
+
+                        if (networkInfo != null && networkInfo.isConnected) {
+                            return true
+                        }
+                        return false
+                    }
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        return true
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        return true
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+    }
+}
