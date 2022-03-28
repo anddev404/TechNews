4c0b5ed0432c3b5228a3ad158b222aed163efdd2
anddev404 <marcinek07@gmail.com>
2022-03-28 18:20:33 +0200

    Create MainViewModelFactory.kt

---
+++ app/src/main/java/com/anddev404/technews/MainViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.anddev404.technews
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.anddev404.repository.Repository
+import java.lang.IllegalArgumentException
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private var repository: Repository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(repository) as T
+        }
+        throw IllegalArgumentException("MainViewModel class not found.")
+    }
+}
