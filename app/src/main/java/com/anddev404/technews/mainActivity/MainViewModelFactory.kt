b12dd26351acf0901f6e0ecc1758fa8a73a0b2e4
anddev404 <marcinek07@gmail.com>
2023-03-09 22:01:07 +0100

    Update MainViewModelFactory.kt

--- app/src/main/java/com/anddev404/technews/mainActivity/MainViewModelFactory.kt
+++ app/src/main/java/com/anddev404/technews/mainActivity/MainViewModelFactory.kt
@@ -8,7 +8,7 @@ import java.lang.IllegalArgumentException
 @Suppress("UNCHECKED_CAST")
 class MainViewModelFactory(private var repository: Repository) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(repository) as T
         }
